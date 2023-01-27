a184dff7a40cb21c11b841a17bdc09ff3621aa03
Elbert Nathanael <elbertwithane@gmail.com>
2023-01-28 02:39:56 +0800

    refactored aged brie

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -23,11 +23,29 @@ class Shop {
         //make sure sulfuras quality=80 and sellIn = -1
         currentItem.quality = 80;
         currentItem.sellIn = -1;
+      }else if(currentItem.name === 'Aged Brie'){
+        // iterate quality
+        currentItem.quality += 1;
+        this.decreaseSellIn(currentItem);
+        this.clampQuality(currentItem)
       }else{
         this.oldUpdateItem(i);
       }
     }
     return this.items;
+  }
+   
+  clampQuality(item){
+    if(item.quality > 50){
+      item.quality = 50
+    }
+    if(item.quality < 0){
+      item.quality = 0
+    }
+  }
+
+  decreaseSellIn(item){
+    item.sellIn -= 1;
   }
    
   oldUpdateItem(i){
