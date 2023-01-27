130d6e76c9727df79cfc2d68d899cf412c080345
Elbert Nathanael <elbertwithane@gmail.com>
2023-01-28 01:07:53 +0800

    finished test code

--- notes.md
+++ notes.md
@@ -1,24 +1,76 @@
-- its an inn inventory
-- holds iteum class
-    - quality: is value
-    - sell in: is like expiry
-    - special items and speicla depreciatin rate
-- new conjured item type
-    - depreciate twice as fast
-    
+# understanding what the hell is going on
+the requirement
+    - it's an inventory manifests that updates the day's property as the day passes
+    - need to add new feature: allow new categories of items
 
-- the item class
-     new item(name, sellIn, quality)
-     - quality is never negative
-     - quality is never more than 50
+The system
+    - has quality value
+    - all items have a `sellIn` days value. like an expiry date
+    - items have quality
+    - at the end of each day the the system updates all item's sellIn value and quality
 
-    
-- unique items
-aged brie increases quality each day
-sulfuras has no selling date or decrease in quality. max quality at 80
-backstage pass diff value 
+after sell date is passed
+    - items depereciate twice as fast past the sell date
+    - quality is never negative or more than 50
+
+unique item
+    - aged brie increases quality as it gets older
+    - sulfuras, never has to be solde or decrease in quality.. quality is always at 80
+    - backstage passes increases quality towards sellIn but is 0 after concert
+
+The new feature: conjured items
+    - degrade faster then normal items
+
+dos and donts
+    - you can change updateQuality() and add new code.
+    - do not alter the item class or items property
+
+# Testing plan
+    - handling basic item
+        - depreciates quality and sell in days correctly
+        - after sell date is passed. quality depreciates twice as fast
+        - quality clamped at 0 - 50
+    - handling special items
+        - aged brie increases quality regardless
+        - sulfuras doesnt age or depreciate
+        - backstage pass increases value
+            - > 10 increase by 1
+            - <= 10 increase by 2
+            - <= 5 increase by 3
+            - < 0 quality becomes 0
+    - conjured items
+        - quality decrease twice as fast
+
+# what is given in the git repo
+- in src is the class code
+- text tes is.... exmaple of running the class
+
+# so basically
+- just fix the updateQuality()
+- make sure in can add conjure items
 
 
-# todo
-todo create test for usual item. 
-create test for every unique item
+# parsing the original code
+.updateQuality(){
+    for every item:
+        **This handles normal item** 
+        *if* not "aged brie" or "backstage pass":{ 
+            if item quality > 0:
+                if not sulfuras:
+                    item quality -1
+        }
+        **else handle items quality past 50** 
+        else{
+            sdd            
+        }
+
+        **decreaase sell in for all items except sulfuras**
+        *if* not sulfuras{
+            decrease sell in item
+        }
+
+        **handle stuff pass sell in**
+        *if* already past sell in code{
+
+        }
+}
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,149 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+function passTheDay(shop, days){
+  for(let index = 0; index < days; index++) {
+    shop.updateQuality();
+  }
+}
+
+describe('Testing normal items',()=>{
+  //setup shop and item
+  let items;
+  let gildedRose;
+  beforeEach(()=>{
+    items = [
+      new Item("Item", 5, 10),
+    ]
+    gildedRose = new Shop(items);
+  })
+
+  it('Should add items in correctly', ()=>{
+    let pass = true
+    for (let index = 0; index < gildedRose.items.length; index++) {
+      if(gildedRose.items[index] !== items[index]){
+        pass = false
+      }
+    }
+    expect(pass).toEqual(true);
+  })
+  it('Items should decrease in quality', ()=>{
+    passTheDay(gildedRose, 3)
+    expect(gildedRose.items[0].quality).toEqual(7)
   });
-});
+  it('Items sellIn days should decrease', ()=>{
+    passTheDay(gildedRose, 3)
+    expect(gildedRose.items[0].sellIn).toEqual(2)
+  });
+  it('Past the sell date items depreciate twice as fast', ()=>{
+    passTheDay(gildedRose, 7)
+    expect(gildedRose.items[0].quality).toEqual(1)
+  });
+  it('Item quality are clamped at 0', ()=>{
+    passTheDay(gildedRose, 100)
+    expect(gildedRose.items[0].quality).toEqual(0)
+  });
+})
+ 
+describe('Testing aged brie', ()=>{
+  //setup shop and item
+  let items;
+  let gildedRose;
+  beforeEach(()=>{
+    items = [
+      new Item("Aged Brie", 10, 0),
+    ]
+    gildedRose = new Shop(items);
+  })
+
+  it('Aged brie increase quality as days go by', ()=>{
+    passTheDay(gildedRose, 10);
+    expect(gildedRose.items[0].quality).toEqual(10)
+  });
+  it('Aged brie sellIn number goes down as days go by', ()=>{
+    passTheDay(gildedRose, 10);
+    expect(gildedRose.items[0].sellIn).toEqual(0)
+  });
+  it('Aged brie quality is capped at 50', ()=>{
+    passTheDay(gildedRose, 100);
+    expect(gildedRose.items[0].quality).toEqual(50)
+  });
+})
+
+describe('Testing sulfuras', ()=>{
+  //setup shop and item
+  let items;
+  let gildedRose;
+  beforeEach(()=>{
+    items = [
+      new Item("Sulfuras, Hand of Ragnaros", 0, 880),
+    ]
+    gildedRose = new Shop(items);
+  })
+
+  it('Sulfuras quality is always 80', ()=>{
+    passTheDay(gildedRose, 10);
+    expect(gildedRose.items[0].quality).toEqual(80)
+  });
+  it('Sulfuras quality is always -1', ()=>{
+    passTheDay(gildedRose, 10);
+    expect(gildedRose.items[0].sellIn).toEqual(-1)
+  });
+})
+
+describe('Testing backstage concert', ()=>{
+  //setup shop and item
+  let items;
+  let gildedRose;
+  beforeEach(()=>{
+    items = [
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    ]
+    gildedRose = new Shop(items);
+  })
+
+  it('Quality increases by 1 as days go by', ()=>{
+    passTheDay(gildedRose, 5);
+    expect(gildedRose.items[0].quality).toEqual(25)
+  });
+  it('Quality increases by 2 when <11 days to sell date', ()=>{
+    passTheDay(gildedRose, 7);
+    expect(gildedRose.items[0].quality).toEqual(29)
+  });
+  it('Quality increases by 3 when <6 days to sell date', ()=>{
+    passTheDay(gildedRose, 7);
+    expect(gildedRose.items[0].quality).toEqual(29)
+  });
+  it('Quality increases by 3 when <6 days to sell date', ()=>{
+    passTheDay(gildedRose, 14);
+    expect(gildedRose.items[0].quality).toEqual(47)
+  });
+  it('Past the selling date, the quality is zero', ()=>{
+    passTheDay(gildedRose, 140);
+    expect(gildedRose.items[0].quality).toEqual(0)
+  });
+})
+
+describe('Testing conjured items', ()=>{
+  //setup shop and item
+  let items;
+  let gildedRose;
+  beforeEach(()=>{
+    items = [
+      new Item("Conjured Item", 5, 40),
+    ]
+    gildedRose = new Shop(items);
+  })
+
+  it('Conjured items should decrease in quality by 2 each day', ()=>{
+    passTheDay(gildedRose, 2);
+    expect(gildedRose.items[0].quality).toEqual(36)
+  });
+  it('Conjured items should decrease in quality by 4 each day, past sell date', ()=>{
+    passTheDay(gildedRose, 10);
+    expect(gildedRose.items[0].quality).toEqual(20)
+  });
+  it('Item quality are clamped at 0-50', ()=>{
+    passTheDay(gildedRose, 200);
+    expect(gildedRose.items[0].quality).toEqual(0)
+  });
+})
